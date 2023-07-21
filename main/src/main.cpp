0ae810a4af33a6d5d76d8457063a8d75ddbe6a20
Daironn <blizzard@vp.pl>
2023-07-21 16:29:20 +0200

    mongo DB set up

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -9,11 +9,18 @@
 #include "Display.h"
 #include "Scroll.h"
 
+#include "bsoncxx/v_noabi/bsoncxx/builder/stream/document.hpp"
+#include "bsoncxx/v_noabi/bsoncxx/json.hpp"
+#include "bsoncxx/v_noabi/bsoncxx/oid.hpp"
+#include "mongocxx/v_noabi/mongocxx/client.hpp"
+#include "mongocxx/v_noabi/mongocxx/database.hpp"
+#include "mongocxx/v_noabi/mongocxx/uri.hpp"
 
 #include "WiFiManager.h"
 
 extern "C" void app_main(void)
 {
+    
     ESP_ERROR_CHECK(nvs_flash_init());
     tcpip_adapter_init();
 
